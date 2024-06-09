500d081120a8ffa3de4e5f82621577a15ebb4fb6
a.nazarikov <a.nazarikov@tinkoff.ru>
2024-06-09 18:07:18 +0300

    add draft expense

--- backend/src/expense/expense.entity.ts
+++ backend/src/expense/expense.entity.ts
@@ -1,6 +1,7 @@
 import {Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn} from 'typeorm';
 import {Currency} from '../currency/currency.entity';
 import {SplitInfo} from './types';
+import {Event} from "../event/event.entity";
 
 @Entity('expense')
 export class Expense {
@@ -19,10 +20,17 @@ export class Expense {
   @Column()
   currencyId: string;
 
+  @Column()
+  eventId: string;
+
   @Column({type: 'jsonb'})
   splitInformation: string | Array<SplitInfo>;
 
   @ManyToOne(() => Currency)
   @JoinColumn({name: 'currency_id'})
   currency: Currency;
+
+  @ManyToOne(() => Event)
+  @JoinColumn({name: 'event_id'})
+  event: Event;
 }
