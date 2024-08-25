b6c204f4691c2bc4377a360c80cfaf22f522ff60
Rugged-bl <vasia.shumilov@gmail.com>
2024-08-25 23:00:30 +0200

    exchange rates fix

--- android/shared/feature/expenses/src/commonMain/kotlin/com/inwords/expenses/feature/expenses/domain/CurrencyExchanger.kt
+++ android/shared/feature/expenses/src/commonMain/kotlin/com/inwords/expenses/feature/expenses/domain/CurrencyExchanger.kt
@@ -7,12 +7,30 @@ import com.ionspin.kotlin.bignum.decimal.BigDecimal
 internal class CurrencyExchanger {
 
     private val exchangeRates: Map<Pair<String, String>, BigDecimal> = mutableMapOf(
-        Pair("USD", "EUR") to BigDecimal.fromDouble(0.93),
-        Pair("EUR", "USD") to BigDecimal.fromDouble(1.07),
-        Pair("USD", "RUB") to BigDecimal.fromDouble(83.5),
-        Pair("RUB", "USD") to BigDecimal.fromDouble(0.012),
-        Pair("EUR", "RUB") to BigDecimal.fromDouble(89.72),
-        Pair("RUB", "EUR") to BigDecimal.fromDouble(0.011),
+        Pair("USD", "EUR") to BigDecimal.fromDouble(0.89),
+        Pair("USD", "RUB") to BigDecimal.fromDouble(91.13),
+        Pair("USD", "TRY") to BigDecimal.fromDouble(34.01),
+        Pair("USD", "JPY") to BigDecimal.fromDouble(144.30),
+
+        Pair("EUR", "USD") to BigDecimal.fromDouble(1.12),
+        Pair("EUR", "RUB") to BigDecimal.fromDouble(101.98),
+        Pair("EUR", "TRY") to BigDecimal.fromDouble(38.09),
+        Pair("EUR", "JPY") to BigDecimal.fromDouble(161.47),
+
+        Pair("RUB", "USD") to BigDecimal.fromDouble(0.011),
+        Pair("RUB", "EUR") to BigDecimal.fromDouble(0.0098),
+        Pair("RUB", "TRY") to BigDecimal.fromDouble(0.37),
+        Pair("RUB", "JPY") to BigDecimal.fromDouble(1.58),
+
+        Pair("TRY", "USD") to BigDecimal.fromDouble(0.029),
+        Pair("TRY", "EUR") to BigDecimal.fromDouble(0.026),
+        Pair("TRY", "RUB") to BigDecimal.fromDouble(2.68),
+        Pair("TRY", "JPY") to BigDecimal.fromDouble(4.24),
+
+        Pair("JPY", "USD") to BigDecimal.fromDouble(0.0069),
+        Pair("JPY", "EUR") to BigDecimal.fromDouble(0.0062),
+        Pair("JPY", "RUB") to BigDecimal.fromDouble(0.63),
+        Pair("JPY", "TRY") to BigDecimal.fromDouble(0.24)
     )
 
     fun exchange(amount: BigDecimal, fromCurrencyCode: String, toCurrencyCode: String): BigDecimal {
