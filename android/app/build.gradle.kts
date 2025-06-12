4fe6424f8ce30e483baa92392a6965c800544b43
Rugged-bl <vasia.shumilov@gmail.com>
2025-06-12 18:37:54 +0200

    shrink res in release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,9 +22,11 @@ android {
     buildTypes {
         debug {
             isMinifyEnabled = false
+            isShrinkResources = false
         }
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             signingConfig = signingConfigs.getByName("debug")
         }
