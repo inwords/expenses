6391fb496550bb580b77be0f61f486c72191d377
Rugged-bl <vasia.shumilov@gmail.com>
2025-10-06 23:21:45 +0200

    Fix UnsatisfiedLinkError

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "ru.commonex"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "2025.10.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
