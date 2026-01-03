30a20e0a7f0aebf8d00152394a73368f39c6f562
a.nazarikov <a.nazarikov@tinkoff.ru>
2026-01-03 21:53:42 +0300

    feat: добавил шорт линк

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -26,6 +26,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Expenses Swagger')
     .setVersion('0.0.1')
+    .addServer('/api', 'API Server')
     .addApiKey(
       {
         type: 'apiKey',
